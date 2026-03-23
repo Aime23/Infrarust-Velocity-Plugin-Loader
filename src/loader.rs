9c1ee602475e6a566e687d71294f582319e01a94
Deutsch Aimé <contact@aime23.fr>
2026-03-23 13:44:13 +0100

    feat: Add plugin discovery

--- src/loader.rs
+++ src/loader.rs
@@ -1,10 +1,17 @@
+use std::{collections::HashMap, sync::Mutex};
+
 use infrarust_api::{
     event::BoxFuture,
     plugin::{Plugin, PluginMetadata},
 };
 use infrarust_core::plugin::{LoaderError, PluginContextFactory, PluginLoader};
 
-struct PluginLoaderVelocity;
+use crate::plugin::plugin_candidate::PluginCandidate;
+
+struct PluginLoaderVelocity {
+    // Store discovered plugin by id to be able to load them without rescanning every file in the plugin folder
+    plugin_index: Mutex<HashMap<String, PluginCandidate>>,
+}
 
 impl PluginLoader for PluginLoaderVelocity {
     fn name(&self) -> &str {
@@ -15,7 +22,34 @@ impl PluginLoader for PluginLoaderVelocity {
         &'a self,
         plugin_dir: &'a std::path::Path,
     ) -> BoxFuture<'a, Result<Vec<PluginMetadata>, LoaderError>> {
-        todo!()
+        return Box::pin(async move {
+            let dir =
+                plugin_dir
+                    .read_dir()
+                    .map_err(|error| LoaderError::DirectoryNotAccessible {
+                        path: plugin_dir.to_owned(),
+                        source: error,
+                    })?;
+
+            let candidates: Vec<PluginCandidate> = dir
+                .filter_map(|entry| entry.ok())
+                .filter_map(|entry| PluginCandidate::try_from(entry.path().as_path()).ok())
+                .collect();
+
+            let metadata = candidates
+                .iter()
+                .map(|candidate| candidate.metadata().clone().into())
+                .collect();
+
+            // We ignore any lock error because the plugin_index is not essential
+            if let Some(mut plugin_index) = self.plugin_index.lock().ok() {
+                for candidate in candidates {
+                    plugin_index.insert(candidate.metadata().clone().id, candidate);
+                }
+            }
+
+            return Ok(metadata);
+        });
     }
 
     fn load<'a>(
