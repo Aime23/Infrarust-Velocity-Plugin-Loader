730c5b0d71986a317b2d8ca14bdb4e54e892bd97
Deutsch Aimé <contact@aime23.fr>
2026-04-07 19:50:54 +0200

    feat: Add new type handles for PluginContext, PlayerRegistry, ConfigService and Player

--- src/java/handle.rs
+++ src/java/handle.rs
@@ -1,5 +1,7 @@
-use std::{mem::ManuallyDrop};
+use std::{mem::ManuallyDrop, sync::Arc};
 
+use derive_more::{From, Into};
+use infrarust_api::{plugin::PluginContext, prelude::{ConfigService, PlayerRegistry}};
 use jni::sys::jlong;
 
 use crate::handle::Handle;
@@ -18,3 +20,23 @@ where
         Self::from(Handle::from::<T>(instance).raw())
     }
 }
+
+// Define every handle new type here and in the typemap
+
+#[derive(Debug, Into, From)]
+pub struct PluginContextHandle(jlong);
+impl NewTypeHandle<Box<Arc<dyn PluginContext>>> for PluginContextHandle {}
+
+
+#[derive(Debug, Into, From)]
+pub struct PlayerRegistryHandle(jlong);
+impl NewTypeHandle<Box<Arc<dyn PlayerRegistry>>> for PlayerRegistryHandle {}
+
+#[derive(Debug, Into, From)]
+pub struct ConfigServiceHandle(jlong);
+impl NewTypeHandle<Box<Arc<dyn ConfigService>>> for ConfigServiceHandle {}
+
+
+#[derive(Debug, Into, From)]
+pub struct PlayerHandle(jlong);
+impl NewTypeHandle<Box<Arc<dyn infrarust_api::player::Player>>> for PlayerHandle {}
