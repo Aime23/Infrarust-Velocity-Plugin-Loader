acdd74eccc4f0d7a071e9de833db0802d7c89fd0
Deutsch Aimé <contact@aime23.fr>
2026-06-09 16:30:44 +0200

    feat(java): CommonModule for DI of PluginContainer

---
+++ java/infrarust-velocity/src/main/java/dev/infrarust/plugin/CommonModule.java
@@ -0,0 +1,31 @@
+package dev.infrarust.plugin;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
+import com.velocitypowered.api.event.EventManager;
+import com.velocitypowered.api.plugin.PluginContainer;
+import com.velocitypowered.api.plugin.PluginManager;
+import com.velocitypowered.api.proxy.ProxyServer;
+
+public class CommonModule extends AbstractModule {
+
+    private final ProxyServer server;
+    private final PluginContainer container;
+
+    public CommonModule(ProxyServer server, PluginContainer container) {
+        this.server = server;
+        this.container = container;
+    }
+
+    @Override
+    protected void configure() {
+        bind(ProxyServer.class).toInstance(server);
+        bind(PluginManager.class).toInstance(server.getPluginManager());
+        bind(EventManager.class).toInstance(server.getEventManager());
+        // TODO: Uncomment when implemented
+        // bind(CommandManager.class).toInstance(server.getCommandManager());
+        bind(PluginContainer.class)
+            .annotatedWith(Names.named(container.getDescription().getId()))
+            .toInstance(container);
+    }
+}
