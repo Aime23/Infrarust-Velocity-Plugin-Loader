61500d7817ef8d2512a4756f602a88a425a560f3
Deutsch Aimé <contact@aime23.fr>
2026-08-02 14:42:57 +0200

    feat(ivpl-java): Replace load_jar with setup_class_loader an use custom class loader from ivpl-java-classloader

--- crates/ivpl-java/src/lib.rs
+++ crates/ivpl-java/src/lib.rs
@@ -1,6 +1,7 @@
+use ivpl_java_classloader::{BYTE_ARRAY_CLASS_LOADER_BYTES, ByteArrayClassLoader};
 use jni::{
+    jni_str,
     objects::{JByteArray, JClassLoader},
-    strings::JNIString,
 };
 
 use ivpl_java_binding::{
@@ -10,36 +11,18 @@ use ivpl_java_binding::{
 pub const JAR_BYTES: &[i8] =
     unsafe { std::mem::transmute::<&[u8], &[i8]>(include_bytes!(env!("JAR_PATH"))) };
 
-pub const CLASS_EXT: &str = ".class";
+pub fn setup_class_loader<'local>(
+    env: &mut jni::Env<'local>,
+) -> jni::errors::Result<ByteArrayClassLoader<'local>> {
+    let system_class_loader = JClassLoader::get_system_class_loader(env)?;
 
-pub fn load_jar(env: &mut jni::Env) -> jni::errors::Result<()> {
-    let byte_array = JByteArray::new(env, JAR_BYTES.len())?;
-    byte_array.set_region(env, 0, JAR_BYTES as &[i8])?;
-    let bais = ByteArrayInputStream::new(env, byte_array)?;
-    let jis = JarInputStream::new(env, bais)?;
+    env.define_class(
+        Some(jni_str!("ByteArrayClassLoader")),
+        &system_class_loader,
+        BYTE_ARRAY_CLASS_LOADER_BYTES,
+    )?;
 
-    loop {
-        let entry = jis.get_next_jar_entry(env)?;
-        if entry.is_null() {
-            break;
-        }
-        let name = entry.get_name(env)?;
-        let name = name.try_to_string(env)?;
-        let ext_pos = name.rfind(CLASS_EXT).unwrap_or_default();
-        if (name.len() > CLASS_EXT.len() && ext_pos == name.len() - CLASS_EXT.len()) {
-            let class_bytes = jis.as_input_stream().read_all_bytes(env)?;
-            let class_bytes = env.convert_byte_array(class_bytes)?;
-            let mut class_name = name.clone();
-            class_name.replace_range(ext_pos..name.len(), "");
-            let system_class_loader = JClassLoader::get_system_class_loader(env)?;
-            let jclass_name = JNIString::new(class_name);
-            let cls = env.define_class(Some(jclass_name), system_class_loader, &class_bytes)?;
-            let loaded_class_name = {
-                let loaded_class_name = cls.get_name(env)?;
-                loaded_class_name.try_to_string(env)?
-            };
-            println!("Loaded {} into {}", &name, &loaded_class_name);
-        }
-    }
-    return Ok(());
+    let value = JByteArray::new(env, JAR_BYTES.len())?;
+    value.set_region(env, 0, JAR_BYTES)?;
+    return ByteArrayClassLoader::new(env, value, system_class_loader);
 }
