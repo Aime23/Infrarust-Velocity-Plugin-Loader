652bba9494422b9a2737e03f05ee4f4c081f146e
Deutsch Aimé <contact@aime23.fr>
2026-08-07 12:55:41 +0200

    fix(ivpl-java): invalid InvoundConnection impl

--- crates/ivpl-java/infrarust-velocity/src/main/java/dev/infrarust/proxy/InfrarustInboundConnection.java
+++ crates/ivpl-java/infrarust-velocity/src/main/java/dev/infrarust/proxy/InfrarustInboundConnection.java
@@ -8,71 +8,68 @@ import com.velocitypowered.api.network.ProtocolState;
 import com.velocitypowered.api.network.ProtocolVersion;
 import com.velocitypowered.api.proxy.InboundConnection;
 
-public class InfrarustInboundConnection implements Inbo
+public class InfrarustInboundConnection implements InboundConnection {
 
-private inal InetSocketAddress remoteAddress;private final InetSocketAddress virtualHost;private final String rawVirtualHost;private final ProtocolVersion protocolVersion;private final ProtocolState protocolState;private final HandshakeIntent handshakeIntent;
+    private final InetSocketAddress remoteAddress;
+    private final InetSocketAddress virtualHost;
+    private final String rawVirtualHost;
+    private final ProtocolVersion protocolVersion;
+    private final ProtocolState protocolState;
+    private final HandshakeIntent handshakeIntent;
 
 
-public InfrarustInboundConnection(final InetSocketAddress remoteAddress,final ProtocolVersion protocolVersion,final ProtocolState protocolState,final HandshakeIntent handshakeIntent){this(remoteAddress,null,null,protocolVersion,protocolState,handshakeIntent);}
+    public InfrarustInboundConnection(final InetSocketAddress remoteAddress,
+            final ProtocolVersion protocolVersion, final ProtocolState protocolState,
+            final HandshakeIntent handshakeIntent) {
+        this(remoteAddress, null, null, protocolVersion, protocolState, handshakeIntent);
+    }
 
-public InfrarustInboundConnection(final InetSocketAddress remoteAddress,@Nullable final InetSocketAddress virtualHost,@Nullable final String rawVirtualHost,final ProtocolVersion protocolVersion,final ProtocolState protocolState,final HandshakeIntent handshakeIntent){this.remoteAddress=remoteAddress;this.virtualHost=virtualHost;this.rawVirtualHost=rawVirtualHost;this.protocolVersion=protocolVersion;this.protocolState=protocolState;this.handshakeIntent=handshakeIntent;}
+    public InfrarustInboundConnection(final InetSocketAddress remoteAddress,
+            @Nullable final InetSocketAddress virtualHost, @Nullable final String rawVirtualHost,
+            final ProtocolVersion protocolVersion, final ProtocolState protocolState,
+            final HandshakeIntent handshakeIntent) {
+        this.remoteAddress = remoteAddress;
+        this.virtualHost = virtualHost;
+        this.rawVirtualHost = rawVirtualHost;
+        this.protocolVersion = protocolVersion;
+        this.protocolState = protocolState;
+        this.handshakeIntent = handshakeIntent;
+    }
 
-@Override public InetSocketAddress getRemoteAddress(){return this.remoteAddress;}
+    @Override
+    public InetSocketAddress getRemoteAddress() {
+        return this.remoteAddress;
+    }
 
-@Override public Optional<InetSocketAddress>getVirtualHost(){return Optional.ofNullable(this.virtualHost);}
+    @Override
+    public Optional<InetSocketAddress> getVirtualHost() {
+        return Optional.ofNullable(this.virtualHost);
+    }
 
-@Override public Optional<String>getRawVirtualHost(){return Optional.ofNullable(this.rawVirtualHost);}
+    @Override
+    public Optional<String> getRawVirtualHost() {
+        return Optional.ofNullable(this.rawVirtualHost);
+    }
 
-@Override public boolean isActive(){return true;}
+    @Override
+    public boolean isActive() {
+        return true;
+    }
 
-@Override public ProtocolVersion getProtocolVersion(){return this.protocolVersion;}
+    @Override
+    public ProtocolVersion getProtocolVersion() {
+        return this.protocolVersion;
+    }
 
-@Override public ProtocolState getProtocolState(){return this.protocolState;}
+    @Override
+    public ProtocolState getProtocolState() {
+        return this.protocolState;
+    }
 
-@Override public HandshakeIntent getHandshakeIntent(){return this.handshakeIntent;}
+    @Override
+    public HandshakeIntent getHandshakeIntent() {
+        return this.handshakeIntent;
+    }
 
 
 }
- 
-
-    
-    
-        
-        
-    
-
-    
-    
-        
-        
-    
-
-    
-    
-        
-        
-    
-
-    
-    
-        
-        
-    
-
-    
-    
-        
-        
-    
-
-    
-    
-        
-        
-    
-
-    
-    
-        
-        
-    
