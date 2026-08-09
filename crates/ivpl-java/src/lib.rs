2a4bb20f6c535ea32ffd974ed357a47c5d358dbc
Deutsch Aimé <contact@aime23.fr>
2026-08-09 19:48:46 +0200

    refactor(ivpl-java): Split impl to allow loading from own bytes

--- crates/ivpl-java/src/lib.rs
+++ crates/ivpl-java/src/lib.rs
@@ -7,18 +7,31 @@ use jni::{
 pub const JAR_BYTES: &[i8] =
     unsafe { std::mem::transmute::<&[u8], &[i8]>(include_bytes!(env!("JAR_PATH"))) };
 
+pub trait FromBytes<'local>: Sized {
+    fn from_bytes(env: &mut jni::Env<'local>, bytes: &[i8]) -> jni::errors::Result<Self>;
+}
+
+impl<'local> FromBytes<'local> for ByteArrayClassLoader<'local> {
+    fn from_bytes(
+        env: &mut jni::Env<'local>,
+        bytes: &[i8],
+    ) -> jni::errors::Result<ByteArrayClassLoader<'local>> {
+        let system_class_loader = JClassLoader::get_system_class_loader(env)?;
+
+        env.define_class(
+            Some(jni_str!("ByteArrayClassLoader")),
+            &system_class_loader,
+            BYTE_ARRAY_CLASS_LOADER_BYTES,
+        )?;
+
+        let value = JByteArray::new(env, bytes.len())?;
+        value.set_region(env, 0, bytes)?;
+        return ByteArrayClassLoader::new(env, value, system_class_loader);
+    }
+}
+
 pub fn setup_class_loader<'local>(
     env: &mut jni::Env<'local>,
 ) -> jni::errors::Result<ByteArrayClassLoader<'local>> {
-    let system_class_loader = JClassLoader::get_system_class_loader(env)?;
-
-    env.define_class(
-        Some(jni_str!("ByteArrayClassLoader")),
-        &system_class_loader,
-        BYTE_ARRAY_CLASS_LOADER_BYTES,
-    )?;
-
-    let value = JByteArray::new(env, JAR_BYTES.len())?;
-    value.set_region(env, 0, JAR_BYTES)?;
-    return ByteArrayClassLoader::new(env, value, system_class_loader);
+    return ByteArrayClassLoader::from_bytes(env, JAR_BYTES);
 }
